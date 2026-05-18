4ca9b4bed42df29d5e9a4986a49c7270569d1477
Vorti <vorti@vorti.com>
2026-05-18 14:43:48 +1000

    Fix all-same-color avatars: use predefined distinct palette instead of HSL hue

--- flutter_app/lib/utils/avatar_utils.dart
+++ flutter_app/lib/utils/avatar_utils.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
+final _avatarColors = [
+  Color(0xFFE53935), // red
+  Color(0xFF1E88E5), // blue
+  Color(0xFF43A047), // green
+  Color(0xFFFB8C00), // orange
+  Color(0xFF8E24AA), // purple
+  Color(0xFF00ACC1), // cyan
+  Color(0xFFD81B60), // pink
+  Color(0xFF3949AB), // indigo
+  Color(0xFF6D4C41), // brown
+  Color(0xFF546E7A), // blue-grey
+  Color(0xFFF4511E), // deep orange
+  Color(0xFF00897B), // teal
+];
+
 Color colorFromId(String id) {
   final hash = id.codeUnits.fold<int>(0, (prev, c) => prev * 31 + c);
-  final hue = hash.abs() % 360;
-  return HSLColor.fromAHSL(1, hue.toDouble(), 0.5, 0.55).toColor();
+  return _avatarColors[hash.abs() % _avatarColors.length];
 }
